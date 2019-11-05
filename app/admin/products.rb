cfd69141c4e794def9c301bec33862b329711e0c
Adrian Mapue <adrianmapue@yahoo.ca>
2019-11-05 01:25:15 -0600

    Created a image browse in active admin

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Product do
-  permit_params :product_Id, :ProductName, :GroupType, :Price, :Description, :Quantity, :ProductDetails, :image, :tracklist
+  permit_params :product_Id, :ProductName, :GroupType, :Price, :Description, :Quantity, :ProductDetails, :tracklist, :image
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
 
   resources :products, only: :show
 
-  root to: 'products#indexra'
+  root to: 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
