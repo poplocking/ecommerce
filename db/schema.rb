42e3ee2be1ac631a7bea66cd874de11da7c7ac79
Adrian Mapue <adrianmapue@yahoo.ca>
2019-12-05 09:28:06 -0600

    Dropped unnecessary Quantity  Column in Product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_05_152339) do
+ActiveRecord::Schema.define(version: 2019_12_05_152524) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -96,7 +96,6 @@ ActiveRecord::Schema.define(version: 2019_12_05_152339) do
   create_table "products", force: :cascade do |t|
     t.integer "product_Id"
     t.string "ProductName"
-    t.string "GroupType"
     t.float "Price"
     t.string "Description"
     t.integer "Quantity"
